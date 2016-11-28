f1b9933050c13c63bdbec38a861f2b6535e11536
Neha Bhandari <GCOMSOFT\NBhandari>
2016-11-28 22:49:27 +0000

    removed otrequestfactory
    git-tfs-id: [http://ev-tfs-dev:8080/tfs/CA_State_Collection/]$/OAKRAP/RAP/MAIN;C103

--- 1.WEBSERVER/FinOT.WebClient/Application/1.Modules/rapModule.js
+++ 1.WEBSERVER/FinOT.WebClient/Application/1.Modules/rapModule.js
@@ -1,7 +1,7 @@
 ﻿'use strict';
 var rapModule = angular.module('rapModule', ['ngFileUpload'])
     
-    .factory('otrequestFactory', otrequestFactory)
+    
     .factory('rapcustFactory', rapcustFactory)
     .factory('rapGlobalFactory', function () {
 
@@ -170,82 +170,4 @@ var rapModule = angular.module('rapModule', ['ngFileUpload'])
         };
     })
     
-    .directive('otHeader', function () {
-        return {
-            restrict: 'E',
-            scope: {
-                reqid: '=',
-                model: '=model',
-            },
-            templateUrl: 'Views/Request/_Header.html',
-            controller: 'otheaderController',
-            controllerAs: 'Ctrl'
-        };
-    })
-    .directive('otRegister', function () {
-        return {
-            restrict: 'E',
-            scope: {
-                reqid: '=',
-                model: '=model',
-            },
-            templateUrl: 'Views/Account/_CreateUser.html',
-            controller: 'otregisterController',
-            controllerAs: 'Ctrl'
-        };
-    })
-    .directive('otStaff', function () {
-        return {
-            restrict: 'E',
-            scope: {
-                reqid: '='
-            },
-            templateUrl: 'Views/Request/_Staff.html',
-            controller: 'otstaffController',
-            controllerAs: 'Ctrl'
-        };
-    })
-    .directive('otFunding', function () {
-        return {
-            restrict: 'E',
-            scope: {
-               reqid: '='
-            },
-            templateUrl: 'Views/Request/_Funding.html',
-            controller: 'otfundingController',
-            controllerAs: 'Ctrl'
-        };
-    })
-    .directive('otApproval', function () {
-        return {
-            restrict: 'E',
-            scope: {
-                reqid: '='
-            },
-            templateUrl: 'Views/Request/_Approval.html',
-            controller: 'otapprovalController',
-            controllerAs: 'Ctrl'
-        };
-    })
-    .directive('otNotes', function () {
-        return {
-            restrict: 'E',
-            scope: {
-                reqid: '='
-            },
-            templateUrl: 'Views/Request/_Notes.html',
-            controller: 'otnotesController',
-            controllerAs: 'Ctrl'
-        };
-    })
-    .directive('otDocuments', function () {
-        return {
-            restrict: 'E',
-            scope: {
-                reqid: '='
-            },
-            templateUrl: 'Views/Request/_Documents.html',
-            controller: 'otdocumentsController',
-            controllerAs: 'Ctrl'
-        }
-    })
+   
