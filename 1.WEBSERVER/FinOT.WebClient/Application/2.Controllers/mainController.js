d36b0619b4018302cee7c64a3e5238ad275d2673
Neha Bhandari <GCOMSOFT\NBhandari>
2017-01-03 15:31:01 +0000

    login and logout functionality
    git-tfs-id: [http://ev-tfs-dev:8080/tfs/CA_State_Collection/]$/OAKRAP/RAP/MAIN;C315

--- 1.WEBSERVER/FinOT.WebClient/Application/2.Controllers/mainController.js
+++ 1.WEBSERVER/FinOT.WebClient/Application/2.Controllers/mainController.js
@@ -1,7 +1,30 @@
-﻿var mainController = ['$scope', '$location', 'Page', 'authFactory', 'alertService', function ($scope, $location, Page, auth, alertService) {
+﻿var mainController = ['$scope', '$location', 'Page', 'authFactory', 'alertService', 'rapGlobalFactory', 'masterdataFactory', function ($scope, $location, Page, auth, alertService, rapGlobalFactory, masterFactory) {
     $scope.Page = Page;
   
-           
+    var self = this;
+    self.UserName = '';
+    
+    
+    self.GetUserName = function ()
+    {
+        if (rapGlobalFactory.CustomerDetails != null)
+        {
+            self.UserName =  rapGlobalFactory.CustomerDetails.User.FirstName;
+        }
+       // self.UserName = 'Neha';
+        return self.UserName;
+    }
+
+    //TBD clear the cache
+    self.LogOut = function ()
+    {
+        self.UserName = '';
+        rapGlobalFactory.CustomerDetails = null;
+        rapGlobalFactory.CustID = 0;
+        rapGlobalFactory.CaseDetails = null;
+        rapGlobalFactory.CityUser = null;
+        $location.path("/loginURL");
+    }
     $location.path("/loginURL");
  
 }];
