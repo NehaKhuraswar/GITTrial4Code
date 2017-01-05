16ec142c075802b2582d8797a3295e7f7912307c
Venky Soundar <GCOMSOFT\VSounder>
2017-01-05 20:13:56 +0000

    Cleanup
    git-tfs-id: [http://ev-tfs-dev:8080/tfs/CA_State_Collection/]$/OAKRAP/RAP/MAIN;C339

--- 2.APPSERVER/FinOT.Business/Helper/DataTableHelper.cs
+++ 2.APPSERVER/FinOT.Business/Helper/DataTableHelper.cs
@@ -80,41 +80,41 @@ namespace RAP.Business.Helper
             return dt;
         }
 
-        public static DataTable CreateDocumentsTable(List<Document> lst)
-        {
-            DataTable dt = new DataTable();
-            dt.Columns.Add("DocID", typeof(int));
-            dt.Columns.Add("SectionID", typeof(int));
-            dt.Columns.Add("KeyID", typeof(int));
-            dt.Columns.Add("AdditionalKey", typeof(string));
-            dt.Columns.Add("CategoryID", typeof(int));
-            dt.Columns.Add("Description", typeof(string));
-            dt.Columns.Add("FileName", typeof(string));
-            dt.Columns.Add("Content", typeof(byte[]));
-            if (lst != null)
-            {
-                foreach (Document item in lst)
-                {
-                    DataRow dr = dt.NewRow();
-                    dr[0] = CheckZeroNullValue(item.DocID);
-                    dr[1] = item.Section.ID;
-                    dr[2] = item.KeyID;
-                    dr[3] = item.AdditionalKey;
-                    if (item.Category != null)
-                    {
-                        dr[4] = item.Category.ID;
-                    }
-                    else
-                    {
-                        dr[4] = 0;
-                    }
-                    dr[5] = CheckEmptyStringValue(item.Description);
-                    dr[6] = item.FileName;
-                    dr[7] = item.Content;
-                    dt.Rows.Add(dr);
-                }
-            }
-            return dt;
-        }
+    //    public static DataTable CreateDocumentsTable(List<Document> lst)
+    //    {
+    //        DataTable dt = new DataTable();
+    //        dt.Columns.Add("DocID", typeof(int));
+    //        dt.Columns.Add("SectionID", typeof(int));
+    //        dt.Columns.Add("KeyID", typeof(int));
+    //        dt.Columns.Add("AdditionalKey", typeof(string));
+    //        dt.Columns.Add("CategoryID", typeof(int));
+    //        dt.Columns.Add("Description", typeof(string));
+    //        dt.Columns.Add("FileName", typeof(string));
+    //        dt.Columns.Add("Content", typeof(byte[]));
+    //        if (lst != null)
+    //        {
+    //            foreach (Document item in lst)
+    //            {
+    //                DataRow dr = dt.NewRow();
+    //                dr[0] = CheckZeroNullValue(item.DocID);
+    //                dr[1] = item.Section.ID;
+    //                dr[2] = item.KeyID;
+    //                dr[3] = item.AdditionalKey;
+    //                if (item.Category != null)
+    //                {
+    //                    dr[4] = item.Category.ID;
+    //                }
+    //                else
+    //                {
+    //                    dr[4] = 0;
+    //                }
+    //                dr[5] = CheckEmptyStringValue(item.Description);
+    //                dr[6] = item.FileName;
+    //                dr[7] = item.Content;
+    //                dt.Rows.Add(dr);
+    //            }
+    //        }
+    //        return dt;
+    //    }
     }
 }
--- 2.APPSERVER/FinOT.Core/DataModels/Document.cs
+++ 2.APPSERVER/FinOT.Core/DataModels/Document.cs
@@ -6,27 +6,6 @@ using System.Threading.Tasks;
 
 namespace RAP.Core.DataModels
 {
-    public class Document : DocumentContent
-    {
-        public int? DocID { get; set; }        
-        public IDDescription Section { get; set; }
-        public int KeyID { get; set; }
-        public string AdditionalKey { get; set; }
-        public IDDescription Category { get; set; }
-        public string Description { get; set; }
-        public int? Version { get; set; }
-        public int? Rank { get; set; }
-        public Audit Audit { get; set; }
-        public bool PostCompleteIndicator { get; set; }
-    }
-
-    public class DocumentContent
-    {
-        public string FileName { get; set; }
-        public byte[] Content { get; set; }
-        public string Extension { get; set; }
-        public string ContentType { get; set; }
-    }
 
      public class DocumentM
      {
