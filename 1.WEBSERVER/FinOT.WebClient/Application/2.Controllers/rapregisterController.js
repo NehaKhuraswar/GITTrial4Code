cfe0d3b78bb209df2af659ddb5bfe48f0cca665a
Neha Bhandari <GCOMSOFT\NBhandari>
2017-02-08 16:52:37 +0000

    Password requirements
    git-tfs-id: [http://ev-tfs-dev:8080/tfs/CA_State_Collection/]$/OAKRAP/RAP/MAIN;C659

--- 1.WEBSERVER/FinOT.WebClient/Application/2.Controllers/rapregisterController.js
+++ 1.WEBSERVER/FinOT.WebClient/Application/2.Controllers/rapregisterController.js
@@ -84,9 +84,7 @@ var rapregisterController = ['$scope', '$modal', 'alertService', 'rapcustFactory
     var checkPassword = function (pwd, email) {
         if (email == pwd)
             return false;
-        //var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\_\-])(?=.{8,})");
-        var strongRegex = new RegExp("^(?=.*[a-zA-Z])(?=.*[0-9])(?=.{8,})");
-        //var strongRegex = new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})");
+        var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@\_\-])(?=.{8,})");
         return strongRegex.test(pwd);
     }
     var checkPhoneNumber = function (phoneNumber) {
