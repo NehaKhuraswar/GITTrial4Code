779e82d596f916337ba527fe0ab1ea15241cda82
Neha Bhandari <GCOMSOFT\NBhandari>
2017-02-14 21:37:38 +0000

    grounds of appeal
    git-tfs-id: [http://ev-tfs-dev:8080/tfs/CA_State_Collection/]$/OAKRAP/RAP/MAIN;C711

--- 1.WEBSERVER/FinOT.WebClient/Application/2.Controllers/rapServingAppealController.js
+++ 1.WEBSERVER/FinOT.WebClient/Application/2.Controllers/rapServingAppealController.js
@@ -63,22 +63,22 @@ var rapServingAppealController = ['$scope', '$q', '$modal', 'alertService', 'rap
         self.Error = "";
         if (model.FirstName==null || model.FirstName == "")
         {
-            self.Error = self.Error + 'Opposing parties first Name is a required field \n';
+            self.Error = self.Error + 'First name is a required field \n';
         }
         if (model.LastName == null || model.LastName == "") {
-            self.Error = self.Error + 'Opposing parties last Name is a required field \n';
+            self.Error = self.Error + 'Last name is a required field \n';
         }
         if (model.AddressLine1 == null || model.AddressLine1 == "") {
-            self.Error = self.Error + 'Opposing parties address line1 is a required field \n';
+            self.Error = self.Error + 'Address line 1 is a required field \n';
         }
         if (model.City == null || model.City == "") {
-            self.Error = self.Error + 'Opposing parties city is a required field \n';
+            self.Error = self.Error + 'City is a required field \n';
         }
         if (model.State == null) {
-            self.Error = self.Error + 'Opposing parties state is a required field \n';
+            self.Error = self.Error + 'State is a required field \n';
         }
         if (model.Zip == null) {
-            self.Error = self.Error + 'Opposing parties zip is a required field \n';
+            self.Error = self.Error + 'Zip is a required field \n';
         }
         if (model.FirstName != "" && model.LastName != "" && model.AddressLine1 != ""
             && model.City != ""
